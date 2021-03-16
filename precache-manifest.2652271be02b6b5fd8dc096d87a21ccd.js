c6aa10a3d127c57c3d3511036dce8ca9a266414e
Weverton Rodrigues <wevertonfr@discente.ufg.br>
2021-03-16 04:10:19 -0300

    Updates

---
+++ precache-manifest.2652271be02b6b5fd8dc096d87a21ccd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda10bca463e02ed8febec41944b10f7",
+    "url": "/wev-gamefolio/index.html"
+  },
+  {
+    "revision": "00148b3187ee510f2ac8",
+    "url": "/wev-gamefolio/static/css/2.e458a1e3.chunk.css"
+  },
+  {
+    "revision": "e08a19fa4cd314c05c06",
+    "url": "/wev-gamefolio/static/css/main.dd7bf384.chunk.css"
+  },
+  {
+    "revision": "00148b3187ee510f2ac8",
+    "url": "/wev-gamefolio/static/js/2.3bc2c7d0.chunk.js"
+  },
+  {
+    "revision": "6c6c4d486690e30c7c2e423e32708a31",
+    "url": "/wev-gamefolio/static/js/2.3bc2c7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08a19fa4cd314c05c06",
+    "url": "/wev-gamefolio/static/js/main.921acb5c.chunk.js"
+  },
+  {
+    "revision": "f335d3da7fa9784e32dc",
+    "url": "/wev-gamefolio/static/js/runtime-main.79dbc71f.js"
+  },
+  {
+    "revision": "490946b8b8aa0cd9449196234c3ad014",
+    "url": "/wev-gamefolio/static/media/PressStart2P-Regular.490946b8.woff"
+  },
+  {
+    "revision": "babe3bc98747e3a8ca2a4e3b7232b741",
+    "url": "/wev-gamefolio/static/media/PressStart2P-Regular.babe3bc9.woff2"
+  }
+]);
