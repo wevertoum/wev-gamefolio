906148fed7c4ea5545894a60613dae2a9c1a5973
weverton <wevertonmixalt@gmail.com>
2022-05-04 15:18:09 -0300

    tirando tipagem inutil

--- src/utils/tipoEntregaLabel.ts
+++
@@ -1,6 +0,0 @@
-const tipoEntregaLabel = {
-  'levar_no_dia': 'levar no casamento',
-  'enviar_domicilio': 'enviar para o endereço',
-} as Record<Models.TipoEntrega, string>;
-
-export default tipoEntregaLabel;
