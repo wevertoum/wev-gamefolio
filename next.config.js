bc4da2571e623a5d6f93d1c1fa481cb146b39563
weverton <wevertonmixalt@gmail.com>
2022-05-04 16:08:31 -0300

    colocando path home pra servir arquivos de estilo

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withAntdLess = require("next-plugin-antd-less");
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withAntdLess({
-  assetPrefix: isProd ? "/wevertoum.github.io/" : "",
+  assetPrefix: "/",
   lessVarsFilePath: "./src/styles/variables.less",
   lessVarsFilePathAppendToEndOfContent: true,
   images: {
