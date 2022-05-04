ba0442d3143a12b5a5d1efdb17852479dbebf35a
weverton <wevertonmixalt@gmail.com>
2022-05-04 16:05:15 -0300

    mudancas pra todar em prod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withAntdLess = require("next-plugin-antd-less");
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = withAntdLess({
+  assetPrefix: isProd ? "/wevertoum.github.io/" : "",
   lessVarsFilePath: "./src/styles/variables.less",
   lessVarsFilePathAppendToEndOfContent: true,
   images: {
