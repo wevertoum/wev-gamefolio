a9bcac74ad86b5678eb9a08e3779730df724002a
weverton <wevertonmixalt@gmail.com>
2022-05-04 15:17:15 -0300

    tirando warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,15 @@
 import "antd/dist/antd.css";
 require("../styles/globals.less");
 import type { AppProps } from "next/app";
-import NProgress from "nprogress";
-import { Router } from "next/router";
 import React from "react";
 import ptBR from "antd/lib/locale/pt_BR";
 import { ConfigProvider } from "antd";
 import "moment/locale/pt-br";
 
-Router.events.on("routeChangeStart", () => {
-  NProgress.start();
-});
-Router.events.on("routeChangeComplete", () => {
-  NProgress.done();
-});
-Router.events.on("routeChangeError", () => {
-  NProgress.done();
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider locale={ptBR}>
+      {/* @ts-ignore */}
       <Component {...pageProps} />
     </ConfigProvider>
   );
